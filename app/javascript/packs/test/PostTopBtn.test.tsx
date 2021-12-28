612fce769617c98b60fb72de9e6066f692b8fc12
tnagae627 <96712704+tnagae627@users.noreply.github.com>
2021-12-28 21:21:28 +0900

    [fix]snapshotをテストに含める

--- app/javascript/packs/test/PostTopBtn.test.tsx
+++ app/javascript/packs/test/PostTopBtn.test.tsx
@@ -1,19 +1,6 @@
 import * as renderer from 'react-test-renderer';
 import { PostTopBtn } from '../post/postTopBtn';
 
-describe('投稿一覧トップボタン：ボタンテスト', () => {
-  const component = renderer.create(<PostTopBtn />);
-  const instance = component.root;
-
-  test('ボタンの要素数確認', () => {
-    expect(instance.findByType('button'));
-  });
-
-  test('ボタンの文言確認', () => {
-    expect(instance.findByProps({type: "button"}).children).toEqual(['戻る']);
-  });
-});
-
 test('投稿一覧トップボタン：コンポーネントテスト', () => {
     const component = renderer.create(<PostTopBtn />);
 
