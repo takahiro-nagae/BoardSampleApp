4d1920ccdca108bfca5155c873365394bfcfce98
tnagae627 <96712704+tnagae627@users.noreply.github.com>
2021-12-29 01:28:56 +0900

    [fix]不要テスト削除

--- app/javascript/packs/test/PostTopBtn.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as renderer from 'react-test-renderer';
-import { PostTopBtn } from '../post/postTopBtn';
-
-test('投稿一覧トップボタン：コンポーネントテスト', () => {
-    const component = renderer.create(<PostTopBtn />);
-
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
