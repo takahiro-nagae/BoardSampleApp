20619e63df9d173fbac54228cb61e03d3aa97974
takahiro-nagae <tnagae627@gmail.com>
2022-01-06 21:05:02 +0900

    [fix]migrateファイル更新

--- db/migrate/20211224155430_create_posts.rb
+++ db/migrate/20211224155430_create_posts.rb
@@ -1,15 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :post_id, null: false
-      t.integer :category_id, null: false
-      t.string  :hide_flag
-      t.string :ip, null: false
-      t.string :name
-      t.string :mail
-      t.string :subject
-      t.text :text, null: false
-      t.timestamps :post_date, null: false
+      t.integer :post_id, null: false, limit: 4
+      t.integer :category_id, null: false, limit: 4
+      t.string  :hide_flag :limit: 1
+      t.string :ip, null: false, limit: 20
+      t.string :name, limit: 256
+      t.string :mail, limit: 256
+      t.string :subject, limit: 256
+      t.text :text, null: false, limit: 256
+      t.datetime :post_date, null: false
 
       t.timestamps
     end
