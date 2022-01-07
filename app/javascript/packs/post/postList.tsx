23e7899518cedc668e03cdf86f6f96d868e46ff2
takahiro-nagae <tnagae627@gmail.com>
2022-01-07 23:16:25 +0900

    [fix]削除カテゴリをアクセスできないように変更

--- app/javascript/packs/post/postList.tsx
+++ app/javascript/packs/post/postList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import * as ReactDOM from 'react-dom';
+import { useEffect, useState, Fragment } from 'react';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import axios from 'axios';
@@ -8,6 +9,7 @@ import { PostData } from './postData'
 import { PostItem } from './postItem'
 import { PostTopBtn } from './postTopBtn';
 import { PostForm } from './postForm';
+import { Error } from '../error/error';
 
 /****************************************
  * 投稿一覧コンポーネント
@@ -29,9 +31,18 @@ export const PostList = (props) => {
             setClientIp(res.data.IPv4);
         });
         // 投稿一覧を取得
-        axios.get<PostData[]>('http://localhost:3000/post/get?category_id=' + query['category_id']).then((res) => {
+        axios.get<PostData[]>('http://localhost:3000/post/get?category_id=' + query['category_id'])
+        .then((res) => {
             setPosts(res.data);
-        });
+        })
+        .catch(error => {
+            if(error.response.status == 422) {
+                ReactDOM.render(
+                    <Error msg={error.response.data.errors} />
+                    , document.getElementById('postContent')
+                );
+            }
+        })
     }, []);
 
     // ================================
@@ -41,23 +52,24 @@ export const PostList = (props) => {
         let newPosts = [...posts];
         setPosts(newPosts);
     }
-
     return(
-        <div>
+        <>
             {/* 戻るボタン */}
             <PostTopBtn />
-            {/* 投稿一覧 */}
-            <div className='py-5'>
-                <ul className='list-group'>
-                    {posts.map(post => (
-                        <PostItem postData={post} updatePosts={updatePosts}
-                         loggedInStatus={loggedInStatus} key={post.id} />
-                    ))}
-                </ul>
+            <div id='postContent'>
+                {/* 投稿一覧 */}
+                <div className='py-5'>
+                    <ul className='list-group'>
+                        {posts.map(post => (
+                            <PostItem postData={post} updatePosts={updatePosts}
+                            loggedInStatus={loggedInStatus} key={post.id} />
+                        ))}
+                    </ul>
+                </div>
+                {/* 投稿フォーム */}
+                <PostForm category_id={query['category_id']} updatePosts={updatePosts}
+                    clientIp={clientIp} loginUser={loginUser} />
             </div>
-            {/* 投稿フォーム */}
-            <PostForm category_id={query['category_id']} updatePosts={updatePosts}
-                 clientIp={clientIp} loginUser={loginUser} />
-        </div>
+        </>
     );
 };
